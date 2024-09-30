8a3f2a32a16e17f911ff785f5c11540272e1f913
Amr Farghaly <amribrahim.amer@gmail.com>
2024-09-30 15:46:27 +0200

    Update params.py

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
-WANDB_PROJECT = "mlops-course-001"
+WANDB_PROJECT = "Semantiv-Segmentation-Model-for-Autonomous-Vehicle"
 ENTITY = None # set this to team name if working in a team
 BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
-RAW_DATA_AT = 'bdd_simple_1k'
-PROCESSED_DATA_AT = 'bdd_simple_1k_split'
+RAW_DATA_AT = 'dataset'
+PROCESSED_DATA_AT = 'dataset_split'
