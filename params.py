64f276bee0d7295cdb5fc7e73a0728a14c7cf955
Amr Farghaly <amribrahim.amer@gmail.com>
2024-09-30 15:57:17 +0200

    Update params.py

--- params.py
+++ params.py
@@ -1,4 +1,4 @@
-WANDB_PROJECT = "Semantiv-Segmentation-Model-for-Autonomous-Vehicle"
+WANDB_PROJECT = "Semantic-Segmentation-Model-for-Autonomous-Vehicle"
 ENTITY = None # set this to team name if working in a team
 BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
 RAW_DATA_AT = 'dataset'
